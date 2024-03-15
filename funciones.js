06f3392c8518542d38e7996fb75fa4969d76a706
jesuryes <161269641+jesuryes@users.noreply.github.com>
2024-03-15 08:30:15 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function saludar() {
+const nombre = "jesus"
+console.log("Hola", nombre);
+}
+
+function saudarDos(){
+     const nombreDos=prompt("Ingresar un nombre");
+     console.log(Hola, nombreDos);
+}
